c163465039aca36c4d1d81d25777831f371e8fa5
alhabib911 <abdullahalhabib100@gmail.com>
2022-03-26 11:47:40 +0600

    responsive done

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -4,12 +4,19 @@ import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([])
-
+    const [cart, setCart] =useState ([])
     useEffect( () =>{
         fetch('products.json')
         .then(res=> res.json())
         .then(data => setProducts(data))
     },[]);
+
+    const handleAddtoCart = (product) => {
+        // console.log(product);
+        const newCart = [...cart ,product]
+        // console.log(newCart);
+        setCart(newCart)
+    }
 
     return (
         <div className='shop-container'>
@@ -17,11 +24,24 @@ const Shop = () => {
                 {
                     products.map(products=><Product key={products.id}
                         product={products}
+                        handleAddtoCart={handleAddtoCart}
                         ></Product>)
                 }
             </div>
             <div className="order-summery">
-                <h3>Order Summery</h3>
+                <h3>Express Delivery</h3>
+                
+                <div className='product-list'>
+                    {
+                       cart.map((product) =>
+                        <h5 key={product.id}>{product}</h5>
+                        )
+                    }
+                </div>
+                    <button className='rendom-btn'>
+                        <p>Chose 1 for me</p>           
+                    </button>    
+                
             </div>
         </div>
     );
